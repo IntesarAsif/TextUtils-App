af5de901a26fd2173ea79777051c0c3841727a1e
Md Asif Intesar <114006252+IntesarAsif@users.noreply.github.com>
2024-09-16 16:22:54 +0600

    router adding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //wheather dark mode is enabled or not
@@ -33,12 +39,17 @@ function App() {
   return (
 
     <>
-      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container my-3">
+          <Routes>
+              <Route exact path="/about" element={<About />} /> 
+              <Route exact path="/" element ={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>} />
+               
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
